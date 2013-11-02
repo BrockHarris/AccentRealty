1c7f9e77c0536107d771aa99e61db01019c14111
Brock <btharris781@gmail.com>
2013-11-02 16:49:05 -0400

    Adding all

---
+++ app/models/blogposts.rb
@@ -0,0 +1,3 @@
+class Blogposts < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccentRealty::Application.config.secret_token = '7560cbb8f1b1a88124e5c73d6b221205f5cba9bd1de291e3c9d9c6013347a219a8df0fad8d636b6c9556c62a9b2f7680bd324d32acb292edccf8cdc1177d38f2'
