44566ac1047abca374d36e5eaf57bf5b6ae4acc3
Brock <btharris781@gmail.com>
2014-01-31 00:25:38 -0500

    testimonial section

--- app/assets/javascripts/testimonial_slider.js
+++ app/assets/javascripts/testimonial_slider.js
@@ -1,52 +1,41 @@
-$(function(){
-
-  var $clientcarousel = $('#clients-list');
-  var clients = $clientcarousel.children().length;
-  var clientwidth = (clients * 140); // 140px width for each client item 
-  $clientcarousel.css('width',clientwidth);
-  
+$(function(){ 
   var rotating = true;
-  var clientspeed = 1800;
+  var clientspeed = 4500;
   var seeclients = setInterval(rotateTestimonials, clientspeed);
 
   $(document).on({
-    mouseenter: function(){
-      rotating = false; // turn off rotation when hovering
-    },
-    mouseleave: function(){
-      rotating = true;
-    }
+    mouseenter: function(){ rotating = false ;}, // Rotation off when hovering  
+    mouseleave: function(){ rotating = true ;}
   }, '#testimonials');
 
   function rotateTestimonials() {
     if(rotating != false) {
       var $first = $('#testimonial-list li:first');
-      $first.animate({ 'margin-left': '-415px' }, 600, function() {
+      $first.animate({ 'margin-left': '-815px' }, 600, function() {
         $first.remove().css({ 'margin-left': '0px' });
         $('#testimonial-list li:last').after($first);
       });
     }
   }
 
-
-  // vars for testimonials carousel
+  // variables for testimonials carousel
   var $txtcarousel = $('#testimonial-list');
   var txtcount = $txtcarousel.children().length;
-  var wrapwidth = (txtcount * 415) + 415; // 400px width for each testimonial item
+  var wrapwidth = (txtcount * 815) + 815; // 400px width for each testimonial item
   $txtcarousel.css('width',wrapwidth);
   var animtime = 750; // milliseconds for clients carousel
  
-  // prev & next btns for testimonials
-  $('#prv-testimonial').on('click', function(){
+  // nav buttons for testimonials
+  $('#previous_testimonial').on('click', function(){
     var $last = $('#testimonial-list li:last');
-    $last.remove().css({ 'margin-left': '-415px' });
+    $last.remove().css({ 'margin-left': '-815px' });
     $('#testimonial-list li:first').before($last);
     $last.animate({ 'margin-left': '0px' }, animtime); 
   });
   
-  $('#nxt-testimonial').on('click', function(){
+  $('#next_testimonial').on('click', function(){
     var $first = $('#testimonial-list li:first');
-    $first.animate({ 'margin-left': '-415px' }, animtime, function() {
+    $first.animate({ 'margin-left': '-815px' }, animtime, function() {
       $first.remove().css({ 'margin-left': '0px' });
       $('#testimonial-list li:last').after($first);
     });  
